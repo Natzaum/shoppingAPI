4825921eaede2d9f3ac62f26579ce68fd76bb63c
Natzaum <natacezerbordignon@gmail.com>
2024-11-12 16:02:33 -0300

    Exception handler

--- src/main/java/com/placasdevideo/course/services/UserService.java
+++ src/main/java/com/placasdevideo/course/services/UserService.java
@@ -2,11 +2,15 @@ package com.placasdevideo.course.services;
 
 import com.placasdevideo.course.entities.User;
 import com.placasdevideo.course.repositories.UserRepository;
+import com.placasdevideo.course.services.exceptions.DatabaseException;
 import com.placasdevideo.course.services.exceptions.ResourceNotFoundException;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.dao.DataIntegrityViolationException;
+import org.springframework.dao.EmptyResultDataAccessException;
 import org.springframework.stereotype.Repository;
 import org.springframework.stereotype.Service;
 
+import java.util.EmptyStackException;
 import java.util.List;
 import java.util.Optional;
 
@@ -30,7 +34,14 @@ public class UserService {
     }
 
     public void delete(Long id){
-        repository.deleteById(id);
+        try {
+            repository.deleteById(id);
+        }
+        catch (EmptyResultDataAccessException e){
+            throw new ResourceNotFoundException(id);
+        } catch (DataIntegrityViolationException e) {
+            throw new DatabaseException(e.getMessage());
+        }
     }
 
     public User update(Long id, User obj){
